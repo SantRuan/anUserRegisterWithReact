019382b3e36b9bb0fc4fc2f9d42985dde84ca4f0
Ruan Santiago <ruansanvasconcelos@gmail.com>
2021-06-28 11:53:53 -0300

    Program with build installed

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+
+    <Main icon = "home" title ="Principal"
+            subtitle ="A user resistration with React."
+        >
+            <div className="display-4">Welcome</div>
+            <hr />
+            <p className="mb-0">System to examplify a register in React</p>
+        </Main>
---
+++ src/components/templates/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Developed <i className="fa fa-heart text-danger"> by </i> <strong>
+                Ruan Santiago</strong> at <i className="text-danger"><strong>Rio de Janeiro</strong></i> 
+        </span>
+    </footer>
